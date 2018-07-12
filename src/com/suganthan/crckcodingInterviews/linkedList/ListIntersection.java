78a3f718f8fa914748759bca0beaed2d00118a89
msuganthan <suganthanchandrika@gmail.com>
2018-07-12 09:11:42 +0530

    adding more commits

---
+++ src/com/suganthan/crckcodingInterviews/linkedList/ListIntersection.java
@@ -0,0 +1,14 @@
+package com.suganthan.crckcodingInterviews.linkedList;
+
+/**
+ * Given two singly linked list, determine if the two list intersect.
+ * Return the intersecting node. Note that the intersection is based on reference
+ * not based on value. That is, if the kth node of the first linked list is the exact same
+ * node(by reference) as the jth node of the second linked list, then they are intersecting.
+ * Created by msuganthan on 12/7/18.
+ */
+public class ListIntersection {
+    public static void main(String[] args) {
+
+    }
+}
