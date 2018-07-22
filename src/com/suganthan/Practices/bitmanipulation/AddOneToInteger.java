f29fa84a30bb3b7496e031354d230e9c7b571c41
msuganthan <suganthanchandrika@gmail.com>
2018-07-22 21:52:36 +0530

    bitwise

---
+++ src/com/suganthan/Practices/bitmanipulation/AddOneToInteger.java
@@ -0,0 +1,11 @@
+package com.suganthan.Practices.bitmanipulation;
+
+/**
+ * Created by msuganthan on 22/7/18.
+ */
+public class AddOneToInteger {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(-~x);
+    }
+}
