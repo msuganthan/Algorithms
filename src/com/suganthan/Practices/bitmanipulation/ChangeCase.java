fef81827f3b8a52358698c726f2fc876f8d47ca7
msuganthan <suganthanchandrika@gmail.com>
2018-07-24 08:55:31 +0530

    bitwise

---
+++ src/com/suganthan/Practices/bitmanipulation/ChangeCase.java
@@ -0,0 +1,20 @@
+package com.suganthan.Practices.bitmanipulation;
+
+/**
+ * Created by msuganthan on 24/7/18.
+ */
+public class ChangeCase {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000000; i++) {
+            for (char ch='A'; ch <= 'Z'; ch++) {
+                String.to
+                System.out.println(Character.toChars(ch | ' '));
+            }
+        }
+
+
+        /*for (char ch='a'; ch <= 'z'; ch++) {
+            System.out.println(Character.toChars(ch & '_'));
+        }*/
+    }
+}
